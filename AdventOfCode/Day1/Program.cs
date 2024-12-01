ad0f5d715dcd464431cdd78a6905a4bc86aebd79
Chris Daly <chrisdalyenq@gmail.com>
2024-11-30 21:20:05 -0800

    Puzzle 1A solved

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// List<int> left = [3, 4, 2, 1, 3, 3];
+// List<int> right = [4, 3, 5, 3, 9, 3];
+
+var fileText = File.ReadAllText("input.txt");
+var lines = fileText.Split("\n").Where(s => !string.IsNullOrWhiteSpace(s));
+List<int> left = [];
+List<int> right = [];
+foreach (var line in lines)
+{
+    var values = line.Split("   ").Select(int.Parse).ToList();
+    left.Add(values[0]);
+    right.Add(values[1]);
+}
+
+CalculateDifferences();
+return;
+
+void CalculateDifferences()
+{
+    var differences = left.Order().Zip(right.Order(), (l, r) => Math.Abs(l - r));
+
+    Console.WriteLine("Result:");
+    Console.WriteLine(differences.Sum());
+}
