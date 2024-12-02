292b7b5a24f607f11f0ab82a353f86692366f6c9
Chris Daly <chrisdalyenq@gmail.com>
2024-12-01 22:45:25 -0800

    Puzzle 2A solved

---
+++ Day2/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+// List<List<int>> reports = [
+//     [7, 6, 4, 2, 1],
+//     [1, 2, 7, 8, 9],
+//     [9, 7, 6, 2, 1],
+//     [1, 3, 2, 4, 5],
+//     [8, 6, 4, 4, 1],
+//     [1, 3, 6, 7, 9],
+// ];
+
+var reports = LoadReports();
+
+Console.WriteLine("Result:");
+Console.WriteLine(reports.Count(ReportChecker.IsSafe));
+return;
+
+IEnumerable<IEnumerable<int>> LoadReports()
+{
+    var lines = File.ReadAllLines("input.txt");
+    return lines
+        .Where(line => !string.IsNullOrWhiteSpace(line))
+        .Select(line => line.Split(" ").Select(int.Parse));
+}
+
+internal static class ReportChecker
+{
+    private static DirectionAccumulate InitialState => new(Status.Unknown, null);  
+    private static DirectionAccumulate CheckReport(this IEnumerable<int> report)
+    => report.Aggregate(InitialState, (acc, next) 
+        =>
+        {
+            if (acc.Status == Status.Unsafe)
+            {
+                return acc;
+            }
+            
+            if (acc is { Status: Status.Unknown, Last: null })
+            {
+                return new (Status.Unknown, next);
+            }
+
+            if (acc.Status == Status.Unknown)
+            {
+                return (next - acc.Last) switch
+                {
+                    >= 1 and <= 3 => new DirectionAccumulate(Status.SafeAscending, next),
+                    <= -1 and >= -3 => new DirectionAccumulate(Status.SafeDescending, next),
+                    _ => new DirectionAccumulate(Status.Unsafe, null),
+                };
+            }
+            
+            return acc.Status switch
+            {
+                // Check difference is between 1 and 3 in the expected direction
+                Status.SafeAscending when 1 <= next - acc.Last && next - acc.Last <= 3 => new(Status.SafeAscending,
+                    next),
+                Status.SafeDescending when 1 <= acc.Last - next && acc.Last - next <= 3 => new(Status.SafeDescending,
+                    next),
+                _ => new(Status.Unsafe, null)
+            };
+        });
+
+    internal static bool IsSafe(this IEnumerable<int> report) 
+        => report.CheckReport().Status is Status.SafeAscending or Status.SafeDescending; 
+}
+
+internal enum Status
+{
+    Unknown,
+    Unsafe,
+    SafeAscending,
+    SafeDescending,
+}
+
+internal record DirectionAccumulate(Status Status, int? Last);
