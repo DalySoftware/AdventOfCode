52d9153e712a18ea5c5ca20831d29017b7f47b72
Chris Daly <chrisdalyenq@gmail.com>
2024-12-03 22:22:27 -0800

    Move part 1 into a class

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -5,7 +5,7 @@ var input = File.ReadAllText("input.txt");
 
 var searchMatrix = ConvertToMatrix(input);
 
-FindWords(searchMatrix);
+Part1.FindWords(searchMatrix);
 
 return;
 
@@ -15,59 +15,65 @@ char[][] ConvertToMatrix(string input) =>
         .Select(line => line.ToCharArray())
         .ToArray();
 
-void FindWords(char[][] matrix)
+internal static class Part1
 {
-    List<Direction> directions = [
-        new(-1, -1),
-        new(-1, 0),
-        new(-1, 1),
-        new(0, -1),
-        new(0, 1),
-        new(1, -1),
-        new(1, 0),
-        new(1, 1),
-    ];
-
-    var cells = Enumerable.Range(0, matrix.Length)
-        .SelectMany(_ => Enumerable.Range(0, matrix[0].Length), (x, y) => new Address(x, y))
-        .ToList();
-    
-    var searches = directions.SelectMany(_ => cells, (direction, address) => new DirectionalSearch(direction, address));
-    
-    var found = searches.Count(s => TryFindWord(s, matrix));
-    Console.WriteLine($"Found {found} matches");
-}
-
-bool TryFindWord(DirectionalSearch search, char[][] matrix)
-{
-    var currentAddress = search.Address;
-    
-    var maxX = matrix.Length - 1;
-    var maxY = matrix[0].Length - 1;
-    
-    const string targetWord = "XMAS";
-    foreach (var targetChar in targetWord)
+    internal static void FindWords(char[][] matrix)
     {
-        if (!currentAddress.WithinBounds(maxX, maxY)
-            || matrix[currentAddress.X][currentAddress.Y] != targetChar)
-        {
-            return false;
-        }
+        List<Direction> directions = [
+            new(-1, -1),
+            new(-1, 0),
+            new(-1, 1),
+            new(0, -1),
+            new(0, 1),
+            new(1, -1),
+            new(1, 0),
+            new(1, 1),
+        ];
 
-        currentAddress = currentAddress.Move(search.Direction);
+        var cells = Enumerable.Range(0, matrix.Length)
+            .SelectMany(_ => Enumerable.Range(0, matrix[0].Length), (x, y) => new Address(x, y))
+            .ToList();
+        
+        var searches = directions.SelectMany(_ => cells, (direction, address) => new DirectionalSearch(direction, address));
+        
+        var found = searches.Count(s => TryFindWord(s, matrix));
+        Console.WriteLine($"Found {found} matches");
     }
-    
-    return true;
+
+    private static bool TryFindWord(DirectionalSearch search, char[][] matrix)
+    {
+        var currentAddress = search.Address;
+        
+        var maxX = matrix.Length - 1;
+        var maxY = matrix[0].Length - 1;
+        
+        const string targetWord = "XMAS";
+        foreach (var targetChar in targetWord)
+        {
+            if (!currentAddress.WithinBounds(maxX, maxY)
+                || matrix[currentAddress.X][currentAddress.Y] != targetChar)
+            {
+                return false;
+            }
+
+            currentAddress = currentAddress.Move(search.Direction);
+        }
+        
+        return true;
+    }
+
+    private record DirectionalSearch(Direction Direction, Address Address);
 }
 
 internal record Direction(int XIncrement, int YIncrement);
-
 internal record Address(int X, int Y)
 {
     internal bool WithinBounds(int maxX, int maxY) =>
         X >= 0 && X <= maxX && Y >= 0 && Y <= maxY;
 
+    internal bool TouchesBounds(int maxX, int maxY) =>
+        X == 0 || X == maxX || Y == 0 || Y == maxY;
+
     internal Address Move(Direction direction) => new(X + direction.XIncrement, Y + direction.YIncrement);
 }
 
-internal record DirectionalSearch(Direction Direction, Address Address);
