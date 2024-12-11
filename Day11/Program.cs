1e6027c7b28175c11b340444e9298aeda438778e
Chris Daly <chrisdalyenq@gmail.com>
2024-12-11 00:18:56 -0800

    WIP 3

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -5,45 +5,33 @@
 var input = "0 89741 316108 7641 756 9 7832357 91";
 
 var stones = Parse(input);
-// var finalStones = Blink(stones, 75);
+var finalStones = Blink(stones, 25);
 
-var score = 0L;
-foreach (var stone in stones) score += stone.Score();
+var count = 0L;
+foreach (var stone in finalStones) count++;
 
-Console.WriteLine("Score:");
-Console.WriteLine(score);
-
-// Console.WriteLine("Stones:");
+Console.WriteLine("Stones:");
+Console.WriteLine(count);
 // Console.WriteLine(finalStones.LongCount());
 
 return;
 
-// IEnumerable<long> Blink(IEnumerable<long> stones, int blinks)
-// {
-//     var result = stones;
-//     while (blinks > 0)
-//     {
-//         result = result.SelectMany(Extensions.Transform);
-//         blinks--;
-//     }
-//
-//     return result;
-// }
+IEnumerable<string> Blink(IEnumerable<string> stones, int blinks)
+{
+    var result = stones;
+    while (blinks > 0)
+    {
+        result = result.SelectMany(Extensions.Transform);
+        blinks--;
+    }
 
-IEnumerable<Stone> Parse(string input)
-    => input.Split(" ").Select(l => new Stone(long.Parse(l), 0));
+    return result;
+}
+
+IEnumerable<string> Parse(string input) => input.Split(" ");
 
 static class Extensions
 {
-    static long[] Split(this long stone)
-    {
-        var divisor = (long)Math.Pow(10, stone.Digits() / 2);
-        return [stone / divisor, stone % divisor];
-    }
-
-    static bool HasEvenDigits(this long stone)
-        => stone.Digits() % 2 == 0;
-
     static double Digits(this long stone)
     {
         if (stone == 0) return 1;
@@ -60,56 +48,34 @@ static class Extensions
         // return Math.Floor(Math.Log10(stone) + 1);
     }
 
-    static readonly ILookup<long, long> TransformCache = new List<long>().ToLookup(l => l);
+    static readonly Dictionary<string, string[]> TransformCache = new();
 
-    internal static Span<Stone> Transform(Stone stone)
+    internal static IEnumerable<string> Transform(this string stone)
     {
-        if (TransformCache.TryGetValue(stone.Value, out var cached))
-            foreach (var cachedValue in cached)
-                yield return new Stone(cachedValue, stone.Iteration + 1);
+        if (TransformCache.TryGetValue(stone, out var cached))
+            foreach (var str in cached)
+                yield return str;
 
-        if (stone.Value == 0)
+        if (stone == "0")
         {
-            var newStone = new Stone(1, stone.Iteration + 1);
-
-            yield return ;
+            TransformCache[stone] = ["1"];
+            yield return "1";
         }
-        else if (stone.Value.HasEvenDigits())
+        else if (stone.Length % 2 == 0)
         {
-            foreach (var value in stone.Value.Split())
-                yield return new Stone(value, stone.Iteration + 1);
+            var midPoint = stone.Length / 2;
+            var a = stone[..midPoint];
+            var b = stone[midPoint..];
+
+            TransformCache[stone] = [a, b];
+            yield return a;
+            yield return b;
         }
         else
         {
-            yield return new Stone(stone.Value * 2024, stone.Iteration + 1);
+            var result = (long.Parse(stone) * 2024).ToString();
+            TransformCache[stone] = [result];
+            yield return result;
         }
     }
-
-    static readonly Dictionary<Stone, long> ScoreCache = new();
-
-    internal static long Score(this Stone stone)
-    {
-        if (ScoreCache.TryGetValue(stone, out var cachedStoneScore)) return cachedStoneScore;
-
-        var score = 0L;
-
-        var toCalculate = new Stack<Stone>();
-        toCalculate.Push(stone);
-
-        while (toCalculate.TryPop(out var current))
-        {
-            if (current.Iteration >= MaxIterations) score += 1;
-
-            foreach (var newStone in current.Transform()) toCalculate.Push(newStone);
-        }
-
-        ScoreCache[stone] = score;
-        return score;
-    }
-
-    const int MaxIterations = 5;
 }
-
-// record struct Stone(ReadOnlySpan<byte> Value, int Iteration);
-
-ref struct Stone(ReadOnlySpan<byte> Value, int Iteration);
