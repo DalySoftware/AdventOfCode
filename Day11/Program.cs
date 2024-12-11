58146fa6a3e379b6f3a2c090978d98feda9f6ddf
Chris Daly <chrisdalyenq@gmail.com>
2024-12-11 00:40:45 -0800

    WIP 4

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,19 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// var testInput = "125 17";
+﻿// var testInput = "125 17";
 
 var input = "0 89741 316108 7641 756 9 7832357 91";
 
 var stones = Parse(input);
-var finalStones = Blink(stones, 25);
+var finalStones = Blink(stones, 75);
 
 var count = 0L;
-foreach (var stone in finalStones) count++;
+foreach (var _ in finalStones)
+{
+    if (count % 10_000_000 == 0) Console.WriteLine(count);
+    count++;
+}
 
 Console.WriteLine("Stones:");
 Console.WriteLine(count);
-// Console.WriteLine(finalStones.LongCount());
-
 return;
 
 IEnumerable<string> Blink(IEnumerable<string> stones, int blinks)
@@ -21,61 +21,64 @@ IEnumerable<string> Blink(IEnumerable<string> stones, int blinks)
     var result = stones;
     while (blinks > 0)
     {
-        result = result.SelectMany(Extensions.Transform);
+        result = TransformStones(result);
         blinks--;
     }
 
     return result;
 }
 
-IEnumerable<string> Parse(string input) => input.Split(" ");
+static IEnumerable<string> TransformStones(IEnumerable<string> stones)
+{
+    foreach (var stone in stones)
+    foreach (var transformedStone in stone.Transform())
+        yield return transformedStone;
+}
+
+static IEnumerable<string> Parse(string input) => input.Split(" ");
 
 static class Extensions
 {
-    static double Digits(this long stone)
-    {
-        if (stone == 0) return 1;
-
-        var digits = 0;
-
-        while (stone != 0)
-        {
-            stone /= 10;
-            digits++;
-        }
-
-        return digits;
-        // return Math.Floor(Math.Log10(stone) + 1);
-    }
-
     static readonly Dictionary<string, string[]> TransformCache = new();
 
     internal static IEnumerable<string> Transform(this string stone)
     {
         if (TransformCache.TryGetValue(stone, out var cached))
-            foreach (var str in cached)
-                yield return str;
+        {
+            foreach (var str in cached) yield return str;
+
+            yield break;
+        }
 
         if (stone == "0")
         {
             TransformCache[stone] = ["1"];
             yield return "1";
+            yield break;
         }
-        else if (stone.Length % 2 == 0)
+
+        if (stone.Length % 2 == 0)
         {
             var midPoint = stone.Length / 2;
             var a = stone[..midPoint];
-            var b = stone[midPoint..];
+            var b = stone[midPoint..].TruncateLeadingZeros();
 
             TransformCache[stone] = [a, b];
             yield return a;
             yield return b;
+            yield break;
         }
-        else
-        {
-            var result = (long.Parse(stone) * 2024).ToString();
-            TransformCache[stone] = [result];
-            yield return result;
-        }
+
+        var result = (long.Parse(stone) * 2024).ToString();
+        TransformCache[stone] = [result];
+        yield return result;
+    }
+
+    static string TruncateLeadingZeros(this string input)
+    {
+        // Use TrimStart to remove leading zeroes, then ensure at least one character remains
+        var truncated = input.TrimStart('0');
+
+        return string.IsNullOrEmpty(truncated) ? "0" : truncated;
     }
 }
