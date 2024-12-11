41c7f9a8bcf14833c30a6d23ae3fadc9830d4a57
Chris Daly <chrisdalyenq@gmail.com>
2024-12-11 13:50:44 -0800

    WIP arraypool

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,5 +1,7 @@
 ﻿// var testInput = "125 17";
 
+using System.Buffers;
+
 var input = "0 89741 316108 7641 756 9 7832357 91";
 
 var stones = Parse(input);
@@ -24,19 +26,47 @@ long Blink(ReadOnlySpan<string> stones, int blinks)
 
 static ReadOnlySpan<string> TransformStones(ReadOnlySpan<string> stones)
 {
-    var newStones = new List<string>();
-    foreach (var stone in stones)
-    foreach (var transformedStone in stone.Transform())
-        newStones.Add(transformedStone);
+    // Estimate capacity
+    var initialCapacity = (int)Math.Abs(Math.Pow(2, stones.Length));
 
-    return new ReadOnlySpan<string>(newStones.ToArray());
+    // Rent an array from the pool
+    var pool = ArrayPool<string>.Shared;
+    var rentedArray = pool.Rent(initialCapacity);
+    var count = 0;
+
+    try
+    {
+        foreach (var stone in stones)
+        foreach (var transformedStone in stone.Transform())
+        {
+            if (count >= rentedArray.Length)
+            {
+                // Double the size of the array if the capacity is exceeded
+                var newArray = pool.Rent(rentedArray.Length * 2);
+                rentedArray.CopyTo(newArray, 0);
+                pool.Return(rentedArray, true);
+                rentedArray = newArray;
+            }
+
+            rentedArray[count++] = transformedStone;
+        }
+
+        // Return only the used portion as a ReadOnlySpan
+        return new ReadOnlySpan<string>(rentedArray, 0, count);
+    }
+    finally
+    {
+        // Always return the rented array to the pool
+        pool.Return(rentedArray, true);
+    }
 }
 
 static ReadOnlySpan<string> Parse(string input) => input.Split(" ");
 
 static class Extensions
 {
-    static readonly Dictionary<string, ReadOnlyMemory<string>> TransformCache = new();
+    static readonly Dictionary<string, ReadOnlyMemory<string>> TransformCache =
+        new();
 
     internal static ReadOnlySpan<string> Transform(this string stone)
     {
