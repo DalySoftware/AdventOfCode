05974ac002e162fa6c825c67082ac74149915206
Chris Daly <chrisdalyenq@gmail.com>
2024-12-18 14:06:13 -0800

    Puzzle 18A solved

---
+++ Day18/Program.cs
@@ -0,0 +1,119 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// var input = """
+//             5,4
+//             4,2
+//             4,5
+//             3,0
+//             2,1
+//             6,3
+//             2,4
+//             1,5
+//             0,6
+//             3,3
+//             2,6
+//             5,1
+//             1,2
+//             5,5
+//             2,5
+//             6,5
+//             1,4
+//             0,4
+//             6,4
+//             1,1
+//             6,1
+//             1,0
+//             0,5
+//             1,6
+//             2,0
+//             """;
+
+var input = File.ReadAllText("input.txt");
+
+var map = new Map(Parse(input, 1024));
+
+var result = map.ShortestPathToExit();
+
+Console.WriteLine("Result:");
+Console.WriteLine(result);
+
+return;
+
+Entity[] Parse(string input, int limit) => input
+    .Split(["\n", "\r\n"], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+    .Select(
+        line => line.Split(",").Select(int.Parse).ToArray() switch
+        {
+            [var x, var y] => new Byte(new Position(x, y)),
+            _ => throw new ArgumentException("Too many coordinates"),
+        })
+    .Take(limit)
+    .Append<Entity>(new Player(new(0, 0)))
+    .ToArray();
+
+record Position(int X, int Y);
+
+abstract record Entity(Position Position);
+
+record Player(Position Position) : Entity(Position);
+
+record Byte(Position Position) : Entity(Position);
+
+class Map(Entity[] entities)
+{
+    Entity[] Entities { get; } = entities;
+
+    const int MaxX = 70;
+    const int MaxY = 70;
+    Position Exit => new(MaxX, MaxY);
+    Player Player => Entities.OfType<Player>().Single();
+    IEnumerable<Byte> Bytes => Entities.OfType<Byte>();
+
+    internal int ShortestPathToExit() => ShortestPathToExit(Player);
+
+    int ShortestPathToExit(Player player)
+    {
+        var toProcess = new Stack<(Player, int)>();
+        toProcess.Push((player, 0));
+
+        Dictionary<Position, int> minimumCache = new();
+
+        while (toProcess.TryPop(out var current))
+        {
+            if (minimumCache.Count % 10 == 0) Console.WriteLine(minimumCache.Count);
+            // Console.WriteLine(current);
+
+            var (currentPlayer, distance) = current;
+
+            if (minimumCache.TryGetValue(currentPlayer.Position, out var cached) && cached <= distance) continue;
+
+            minimumCache[currentPlayer.Position] = distance;
+
+            foreach (var newPosition in Neighbours(currentPlayer.Position))
+                toProcess.Push((new Player(newPosition), distance + 1));
+        }
+
+        return minimumCache[Exit];
+    }
+
+
+    IEnumerable<Position> Neighbours(Position position) =>
+        PossibleNeighbours(position)
+            .Where(InBounds)
+            .Where(p => !Blocked(p));
+
+    IEnumerable<Position> PossibleNeighbours(Position position)
+    {
+        yield return position with { X = position.X - 1 };
+        yield return position with { X = position.X + 1 };
+        yield return position with { Y = position.Y - 1 };
+        yield return position with { Y = position.Y + 1 };
+    }
+
+
+    bool InBounds(Position position) =>
+        position.X >= 0 && position.X <= MaxX &&
+        position.Y >= 0 && position.Y <= MaxY;
+
+    bool Blocked(Position position) => Bytes.Any(b => b.Position == position);
+};
