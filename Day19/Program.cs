0b434b739628512f2cc1d0614f5facafd8cee547
Chris Daly <chrisdalyenq@gmail.com>
2024-12-18 22:29:06 -0800

    Puzzle 19B solved

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -15,42 +15,60 @@
 
 var input = File.ReadAllText("input.txt");
 
-var (designs, towels) = Parse(input);
+var (designs, towels) = Solver.Parse(input);
 
-var designCount = PossibleDesigns(designs, towels);
+var count = new Solver(designs, towels).NumberOfWays();
 
 Console.WriteLine("Result:");
-Console.WriteLine(designCount);
+Console.WriteLine(count);
 
 return;
 
-int PossibleDesigns(Design[] designs, Towel[] towels) => designs.Count(d => IsPossible(d.Pattern, towels));
-
-bool IsPossible(string pattern, Towel[] towels) => WaysToMakePattern(pattern, towels).Any();
-
-IEnumerable<Towel[]> WaysToMakePattern(string pattern, Towel[] towels)
+class Solver(Design[] designs, Towel[] towels)
 {
-    if (pattern == string.Empty) return [towels];
+    internal long NumberOfWays() =>
+        designs.Sum(d => CachedWaysToMakePattern(d.Pattern));
 
-    var possibleStarts = towels.Where(t => t.MatchesStart(pattern));
-    return possibleStarts.SelectMany(p => WaysToMakePattern(pattern[p.Pattern.Length..], [..towels, p]));
+
+    readonly Dictionary<string, long> _waysCache = new();
+
+    long CachedWaysToMakePattern(string pattern) => _waysCache.TryGetValue(pattern, out var cached)
+        ? cached
+        : _waysCache[pattern] = WaysToMakePattern(pattern);
+
+    long WaysToMakePattern(string pattern)
+    {
+        // Console.WriteLine(pattern);
+        if (pattern == string.Empty) return 1;
+
+        var possibleStarts = towels.Where(t => CachedMatchesStart(t, pattern));
+        return possibleStarts.Sum(p =>
+            CachedWaysToMakePattern(pattern[p.Pattern.Length..]));
+    }
+
+    internal static (Design[] Designs, Towel[] Towels) Parse(string input)
+    {
+        var lines = input.Split(["\r\n", "\n"], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var towels = lines[0].Split(",", StringSplitOptions.TrimEntries).Select(str => new Towel(str))
+            .ToArray();
+
+        var designs = lines[1..].Select(line => new Design(line)).ToArray();
+        return (designs, towels);
+    }
+
+
+    static readonly Dictionary<(Towel, string), bool> MatchesCache = new();
+
+    static bool CachedMatchesStart(Towel towel, string pattern) =>
+        MatchesCache.TryGetValue((towel, pattern), out var cached)
+            ? cached
+            : MatchesCache[(towel, pattern)] = MatchesStart(towel, pattern);
+
+    static bool MatchesStart(Towel towel, string pattern)
+        => towel.Pattern.Length <= pattern.Length &&
+           pattern[..towel.Pattern.Length] == towel.Pattern;
 }
 
-(Design[] Designs, Towel[] Towels) Parse(string input)
-{
-    var lines = input.Split(["\r\n", "\n"], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-    var towels = lines[0].Split(",", StringSplitOptions.TrimEntries).Select(str => new Towel(str))
-        .ToArray();
+readonly record struct Design(string Pattern);
 
-    var designs = lines[1..].Select(line => new Design(line)).ToArray();
-    return (designs, towels);
-}
-
-record Design(string Pattern);
-
-record Towel(string Pattern)
-{
-    internal bool MatchesStart(string pattern) =>
-        Pattern.Length <= pattern.Length &&
-        Pattern.Select((color, index) => (color, index)).All(x => pattern[x.index] == x.color);
-};
+readonly record struct Towel(string Pattern);
